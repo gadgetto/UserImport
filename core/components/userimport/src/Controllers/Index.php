cbabbfe816f9be384036a6a404456d9441f7e5f6
Martin Gartner <martin.gartner@bitego.com>
2023-02-09 19:53:07 +0100

    Added Index manager controller

---
+++ core/components/userimport/src/Controllers/Index.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * This file is part of the UserImport package.
+ *
+ * @copyright bitego (Martin Gartner)
+ * @license GNU General Public License v2.0 (and later)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bitego\UserImport\Controllers;
+
+use MODX\Revolution\modX;
+use Bitego\UserImport\Controllers\Base;
+
+/**
+ * Index manager controller class.
+ *
+ * @param \MODX\Revolution\modX &$modx A reference to the modX object
+ * @param array $config An array of configuration options
+ * @extends Bitego\UserImport\Controllers\Base
+ * @package userimport
+ * @subpackage controllers
+ */
+class Index extends Base
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @access public
+     * @return void
+     */
+    public function loadCustomCssJs()
+    {
+        $this->addJavascript($this->userimport->config['jsUrl'] . 'utils/utilities.js');
+        $this->addJavascript($this->userimport->config['jsUrl'] . 'mgr/widgets/import.panel.js');
+        $this->addJavascript($this->userimport->config['jsUrl'] . 'mgr/widgets/notificationtemplate.panel.js');
+        $this->addJavascript($this->userimport->config['jsUrl'] . 'mgr/widgets/about.panel.js');
+        $this->addLastJavascript($this->userimport->config['jsUrl'] . 'mgr/sections/index.panel.js');
+        $this->addHtml(
+            '<script>
+            Ext.onReady(function(){
+                UserImport.config = ' . $this->modx->toJSON($this->userimport->config) . ';
+                UserImport.request = ' . $this->modx->toJSON($_GET) . ';
+                MODx.add("userimport-panel-index");
+            });
+            </script>'
+        );
+    }
+}
