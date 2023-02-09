2aa7f4ad4f422c626ced8da332f10acb502f3843
Martin Gartner <martin.gartner@bitego.com>
2023-02-09 16:32:29 +0100

    Added composer

---
+++ core/components/userimport/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'bitego/userimport',
+        'pretty_version' => '2.x-dev',
+        'version' => '2.9999999.9999999.9999999-dev',
+        'reference' => 'f5607c55d705cd9da708730686bdbff1d274cc5c',
+        'type' => 'modx-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'bitego/userimport' => array(
+            'pretty_version' => '2.x-dev',
+            'version' => '2.9999999.9999999.9999999-dev',
+            'reference' => 'f5607c55d705cd9da708730686bdbff1d274cc5c',
+            'type' => 'modx-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
