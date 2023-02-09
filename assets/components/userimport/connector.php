d6abf0334c570debd1071945831d3e4c270346ca
Martin Gartner <martin.gartner@bitego.com>
2023-02-09 20:01:59 +0100

    Refactored connector

--- assets/components/userimport/connector.php
+++ assets/components/userimport/connector.php
@@ -1,42 +1,31 @@
 <?php
+
 /**
- * UserImport
+ * This file is part of the UserImport package.
  *
- * Copyright 2014 by bitego <office@bitego.com>
+ * @copyright bitego (Martin Gartner)
+ * @license GNU General Public License v2.0 (and later)
  *
- * UserImport is free software; you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- *
- * UserImport is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this software; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA 02111-1307 USA
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 /**
  * UserImport Connector
  *
+ * @var \MODX\Revolution\modX $modx
+ * @var Bitego\UserImport\UserImport $userimport
  * @package userimport
  */
 
-require_once dirname(dirname(dirname(dirname(__FILE__)))).'/config.core.php';
-require_once MODX_CORE_PATH.'config/'.MODX_CONFIG_KEY.'.inc.php';
-require_once MODX_CONNECTORS_PATH.'index.php';
- 
-$corePath = $modx->getOption('userimport.core_path', null, $modx->getOption('core_path').'components/userimport/');
-require_once $corePath.'model/userimport/userimport.class.php';
-$modx->userimport = new UserImport($modx);
- 
+require_once dirname(__DIR__, 3) . '/config.core.php';
+require_once MODX_CORE_PATH . 'config/' . MODX_CONFIG_KEY . '.inc.php';
+require_once MODX_CONNECTORS_PATH . 'index.php';
+
+$userimport = $modx->services->get('userimport');
 $modx->lexicon->load('userimport:default');
- 
-/* handle request */
-$path = $modx->getOption('processorsPath', $modx->userimport->config, $corePath.'processors/');
-$modx->request->handleRequest(array(
-    'processors_path' => $path,
+
+$modx->request->handleRequest([
+    'processors_path' => $userimport->config['processorsPath'],
     'location' => '',
-));
+]);
