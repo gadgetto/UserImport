324a752ba85fe6b110477e483f6680f27e6f283d
Martin Gartner <martin.gartner@bitego.com>
2023-02-10 11:44:53 +0100

    Prepared default (en) lexicon file for Crowdin

--- core/components/userimport/lexicon/en/default.inc.php
+++ core/components/userimport/lexicon/en/default.inc.php
@@ -1,29 +1,10 @@
 <?php
-/**
- * UserImport
- *
- * Copyright 2014 by bitego <office@bitego.com>
- *
- * UserImport is free software; you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- *
- * UserImport is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this software; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA 02111-1307 USA
- */
 
 /**
  * UserImport default
  *
  * @package userimport
  * @subpackage lexicon
- * @language en
  */
 
 $_lang['userimport']                                            = 'User Import';
