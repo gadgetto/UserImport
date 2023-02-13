bccedae8d1308abb176f8a5128df3bc1bd524039
Martin Gartner <martin.gartner@bitego.com>
2023-02-13 21:18:46 +0100

    Added date validation test

---
+++ _tests/validdate.test.php
@@ -0,0 +1,68 @@
+<?php
+
+use MODX\Revolution\modX;
+use MODX\Revolution\Error\modError;
+use Bitego\UserImport\ImportHandler;
+
+$time_start = microtime_float();
+
+/* Define paths */
+$root = dirname(__DIR__, 1) . '/';
+
+require_once $root . 'config.core.php';
+require_once MODX_CORE_PATH . 'vendor/autoload.php';
+
+/* Load MODX */
+$modx = new modX();
+$modx->initialize('mgr');
+if (!$modx->services->has('error')) {
+    $modx->services->add('error', function ($c) use ($modx) {
+        return new modError($modx);
+    });
+}
+$modx->error = $modx->services->get('error');
+$modx->setLogLevel(modX::LOG_LEVEL_INFO);
+$modx->setLogTarget('ECHO');
+echo '<pre>';
+flush();
+
+$userimport = $modx->services->get('userimport');
+echo '<b>' . $userimport::NAME . ' ' . $userimport::VERSION . ' ' . $userimport::RELEASE . '</b><br><br>';
+
+/********** Start test-code **********/
+
+$ih = new ImportHandler($modx);
+
+$testcases = [
+    '16.10.66',
+    '16.10.1966',
+    '1966-10-16',
+    '1966.10.16',
+    '1966/10/16',
+    '19661016',
+    '1966. 10 16',
+];
+
+
+foreach ($testcases as $date) {
+    if ($ih->validBirthDate($date)) {
+        echo $date . ' is a valid date -> timestamp: ' . strtotime($date) . '<br>';
+    } else {
+        echo $date . ' is not a valid date<br>';
+    }
+}
+
+
+/********** End test-code **********/
+
+$time_end = microtime_float();
+$time = $time_end - $time_start;
+echo '<br><br>';
+echo 'Processing time: ' . $time;
+echo '</pre>';
+
+function microtime_float()
+{
+    list($usec, $sec) = explode(' ', microtime());
+    return ((float)$usec + (float)$sec);
+}
