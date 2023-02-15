f38a95545d26ac4fbb98420c71b1ad5ff3b62e90
Martin Gartner <martin.gartner@bitego.com>
2023-02-15 18:40:18 +0100

    New translations default.inc.php (Polish)

---
+++ core/components/userimport/lexicon/pl/default.inc.php
@@ -0,0 +1,137 @@
+<?php
+
+/**
+ * UserImport default
+ *
+ * @package userimport
+ * @subpackage lexicon
+ */
+
+$_lang['userimport']                                            = '';
+$_lang['userimport.desc']                                       = '';
+$_lang['userimport.cmp_title']                                  = '';
+$_lang['userimport.settings_save_button']                       = '';
+$_lang['userimport.msg_loading_defaults']                       = '';
+$_lang['userimport.msg_loading_defaults_failed']                = '';
+$_lang['userimport.msg_saving_defaults']                        = '';
+$_lang['userimport.msg_saving_defaults_successfull']            = '';
+$_lang['userimport.msg_saving_defaults_failed']                 = '';
+
+$_lang['userimport.about_tab']                                  = '';
+$_lang['userimport.about_credits']                              = '';
+$_lang['userimport.about_credits_modx_community']               = '';
+
+$_lang['userimport.notification_template_tab']                  = '';
+$_lang['userimport.notification_template_tab_desc']             = '';
+$_lang['userimport.notification_template_mail_subject']         = '';
+$_lang['userimport.notification_template_mail_subject_desc']    = '';
+$_lang['userimport.notification_template_mail_body']            = '';
+$_lang['userimport.notification_template_mail_body_desc']       = '';
+$_lang['userimport.notification_mail_options']                  = '';
+$_lang['userimport.notification_mail_format']                   = '';
+$_lang['userimport.notification_mail_format_desc']              = '';
+$_lang['userimport.notification_mail_format_html']              = '';
+$_lang['userimport.notification_mail_format_plaintext']         = '';
+
+$_lang['userimport.import_users_tab']                           = '';
+$_lang['userimport.import_users_tab_desc']                      = '';
+$_lang['userimport.import_users_file']                          = '';
+$_lang['userimport.import_users_file_desc']                     = '';
+$_lang['userimport.import_users_file_button']                   = '';
+$_lang['userimport.import_users_batchsize']                     = '';
+$_lang['userimport.import_users_batchsize_desc']                = '';
+$_lang['userimport.import_users_first_row_headers']             = '';
+$_lang['userimport.import_users_first_row_headers_desc']        = '';
+$_lang['userimport.import_users_delimiter']                     = '';
+$_lang['userimport.import_users_delimiter_desc']                = '';
+$_lang['userimport.import_users_enclosure']                     = '';
+$_lang['userimport.import_users_enclosure_desc']                = '';
+$_lang['userimport.import_users_assign_groups_roles']           = '';
+$_lang['userimport.import_users_groups']                        = '';
+$_lang['userimport.import_users_roles']                         = '';
+$_lang['userimport.import_users_options']                       = '';
+$_lang['userimport.import_users_email_as_username']             = '';
+$_lang['userimport.import_users_email_as_username_desc']        = '';
+$_lang['userimport.import_users_set_importmarker']              = '';
+$_lang['userimport.import_users_set_importmarker_desc']         = '';
+$_lang['userimport.import_users_notify_users']                  = '';
+$_lang['userimport.import_users_notify_users_desc']             = '';
+
+$_lang['userimport.import_users_button_start']                  = '';
+$_lang['userimport.import_users_status']                        = '';
+$_lang['userimport.import_users_msgbox_start_now']              = '';
+$_lang['userimport.import_users_msgbox_start_now_desc']         = '';
+$_lang['userimport.import_users_msg_importing']                 = '';
+$_lang['userimport.import_users_msg_successfull']               = '';
+$_lang['userimport.import_users_msg_failed']                    = '';
+$_lang['userimport.import_users_unique_import_key']             = '';
+$_lang['userimport.import_users_row']                           = '';
+
+$_lang['userimport.import_users_log_prep_csv_import']           = '';
+$_lang['userimport.import_users_log_importing_csv']             = '';
+$_lang['userimport.import_users_log_extended_detected']         = '';
+$_lang['userimport.import_users_log_batchsize']                 = '';
+$_lang['userimport.import_users_log_imported_user']             = '';
+$_lang['userimport.import_users_log_err_user_data']             = '';
+$_lang['userimport.import_users_log_err_user_failed']           = '';
+$_lang['userimport.import_users_log_err_ns_username']           = '';
+$_lang['userimport.import_users_log_err_username_ae']           = '';
+$_lang['userimport.import_users_log_err_ns_email']              = '';
+$_lang['userimport.import_users_log_err_email_ae']              = '';
+$_lang['userimport.import_users_log_err_email_invalid']         = '';
+$_lang['userimport.import_users_log_err_dob_invalid']           = '';
+$_lang['userimport.import_users_log_err_gender_invalid']        = '';
+$_lang['userimport.import_users_log_err_extended_invalid_json'] = '';
+$_lang['userimport.import_users_log_ns_file']                   = '';
+$_lang['userimport.import_users_log_file_upload_failed']        = '';
+$_lang['userimport.import_users_log_wrong_filetype']            = '';
+$_lang['userimport.import_users_log_wrong_delimiter_detected']  = '';
+$_lang['userimport.import_users_log_delimiter_not_detected']    = '';
+$_lang['userimport.import_users_log_wrong_enclosure_detected']  = '';
+$_lang['userimport.import_users_log_enclosure_not_detected']    = '';
+$_lang['userimport.import_users_log_no_class']                  = '';
+$_lang['userimport.import_users_log_ns_batchsize']              = '';
+$_lang['userimport.import_users_log_ns_delimiter']              = '';
+$_lang['userimport.import_users_log_ns_enclosure']              = '';
+$_lang['userimport.import_users_log_ns_grp']                    = '';
+$_lang['userimport.import_users_log_ns_mailsubject']            = '';
+$_lang['userimport.import_users_log_ns_mailbody']               = '';
+$_lang['userimport.import_users_log_finished']                  = '';
+$_lang['userimport.import_users_log_failed']                    = '';
+$_lang['userimport.import_users_log_err_open_file']             = '';
+$_lang['userimport.import_users_log_diff_fields_values_count']  = '';
+
+$_lang['userimport.import_users_log_err_email_max_len']         = '';
+$_lang['userimport.import_users_log_err_username_max_len']      = '';
+$_lang['userimport.import_users_log_err_fullname_max_len']      = '';
+$_lang['userimport.import_users_log_err_phone_max_len']         = '';
+$_lang['userimport.import_users_log_err_mobilephone_max_len']   = '';
+$_lang['userimport.import_users_log_err_address_max_len']       = '';
+$_lang['userimport.import_users_log_err_country_max_len']       = '';
+$_lang['userimport.import_users_log_err_city_max_len']          = '';
+$_lang['userimport.import_users_log_err_state_max_len']         = '';
+$_lang['userimport.import_users_log_err_zip_max_len']           = '';
+$_lang['userimport.import_users_log_err_fax_max_len']           = '';
+$_lang['userimport.import_users_log_err_photo_max_len']         = '';
+$_lang['userimport.import_users_log_err_comment_max_len']       = '';
+$_lang['userimport.import_users_log_err_website_max_len']       = '';
+
+$_lang['userimport.import_users_log_password_autogenerated']    = '';
+$_lang['userimport.import_users_log_password_len']              = '';
+
+$_lang['setting_userimport.delimiter']                          = '';
+$_lang['setting_userimport.delimiter_desc']                     = '';
+$_lang['setting_userimport.enclosure']                          = '';
+$_lang['setting_userimport.enclosure_desc']                     = '';
+$_lang['setting_userimport.autousername']                       = '';
+$_lang['setting_userimport.autousername_desc']                  = '';
+$_lang['setting_userimport.setimportmarker']                    = '';
+$_lang['setting_userimport.setimportmarker_desc']               = '';
+$_lang['setting_userimport.notifyusers']                        = '';
+$_lang['setting_userimport.notifyusers_desc']                   = '';
+$_lang['setting_userimport.mailsubject']                        = '';
+$_lang['setting_userimport.mailsubject_desc']                   = '';
+$_lang['setting_userimport.mailbody']                           = '';
+$_lang['setting_userimport.mailbody_desc']                      = '';
+$_lang['setting_userimport.mail_format']                        = '';
+$_lang['setting_userimport.mail_format_desc']                   = '';
